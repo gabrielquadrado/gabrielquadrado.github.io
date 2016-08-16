bff160661f2984f7c739dbfb3f238a88471d407d
gabrielquadrado <gabrielquadrado18@gmail.com>
2016-08-16 15:55:50 -0300

    final commit

--- matrizRelacionamento/2.js
+++ matrizRelacionamento/2.js
@@ -1,4 +1,4 @@
-var userAndPassword = "os_username=quadrado&os_password=gafiqua123";
+var userAndPassword = "os_username=quadrado&os_password=atlassian123";
 $(document).ready(function(){
 	$.getJSON('https://monitoratecnologia.atlassian.net/rest/api/latest/project?'+userAndPassword, function(projects){
 	    var i;

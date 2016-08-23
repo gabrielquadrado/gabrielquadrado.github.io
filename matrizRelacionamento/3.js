6d3bb356716c6d716911743d2092ba39154fbf6f
gabrielquadrado <gabrielquadrado18@gmail.com>
2016-08-23 09:38:59 -0300

    add dropdown

--- matrizRelacionamento/3.js
+++ matrizRelacionamento/3.js
@@ -78,7 +78,7 @@ function createTable(){
           if(j==0){
             row.insertCell().innerHTML='<div class="dropdown"><button class="dropbtn">'+issues[i].fields.issuelinks[j].type.outward+
             '</button><div class="dropdown-content" id="drop'+i+'"></div></div>';   
-            while(issues[i].fields.issuelinks[j+1].outwardIssue.key == issues[last].key){
+            while(issues[i].fields.issuelinks[j+1]!='undefined' && issues[i].fields.issuelinks[j+1].outwardIssue.key == issues[last].key){
               $("#drop"+i).append('<option>'+issues[i].fields.issuelinks[j].type.outward+'</option>');
               j++;  
             }
@@ -98,7 +98,7 @@ function createTable(){
           if(j==0){
             row.insertCell().innerHTML='<div class="dropdown"><button class="dropbtn">'+issues[i].fields.issuelinks[j].type.inward+
             '</button><div class="dropdown-content" id="drop'+i+'"></div></div>';   
-            while(issues[i].fields.issuelinks[j+1].inwardIssue.key == issues[last].key){
+            while(issues[i].fields.issuelinks[j+1]!='undefined' && issues[i].fields.issuelinks[j+1].inwardIssue.key == issues[last].key){
               $("#drop"+i).append('<option>'+issues[i].fields.issuelinks[j].type.inward+'</option>');
               j++;  
             }

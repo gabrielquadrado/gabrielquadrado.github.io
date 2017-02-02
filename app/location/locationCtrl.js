5e8f6c924206bba86325980db1119b8025f4c9b4
gabrielquadrado <gabrielquadrado18@gmail.com>
2017-02-02 00:35:45 -0200

    change index to social circles

--- app/location/locationCtrl.js
+++
@@ -1,6 +0,0 @@
-angular.module('app').controller('locationCtrl', function($rootScope, $scope, $location){
-	$('div.jvectormap-tip').remove();
-	$('.parallax').parallax();
-	jQuery.scrollSpeed(100, 800);
-	console.log('location',$scope.region);
-});

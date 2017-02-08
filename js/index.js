371bc53afb3086d835d83a5893587cfa82f9350d
gabrielquadrado <gabrielquadrado18@gmail.com>
2017-02-07 22:00:51 -0200

    m

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
-    $(".player").mb_YTPlayer();
-    $('.tooltipped').tooltip({delay: 50});
+    if($.browser.mobile){
+    	$('body').css('background','url(./img/3.png) repeat center center fixed');
+    	$('body').addClass('mobile');
+    } else {
+    	$(".player").mb_YTPlayer();
+    }
+	$('.tooltipped').tooltip({delay: 50});
 });
